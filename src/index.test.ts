f8420f0fac7b854c0dfc9549978cab96fe74c63a
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2021-11-16 09:11:09 +0100

    test: treat false "shouldReuseExisting" case

--- src/index.test.ts
+++ src/index.test.ts
@@ -16,7 +16,7 @@ const buildCrudList = <T extends Record<string, unknown>>() => {
   }
 }
 
-test("queue", async t => {
+test("queue - reuse existing runnning promises (default)", async t => {
   const todosQueue = buildQueue()
   const todosList = buildCrudList()
 
@@ -73,7 +73,73 @@ test("queue", async t => {
           { id: "1", value: "lorem" },
           { id: "2", value: "ipsum" },
         ],
-        "given [all jobs finished] should [resolve in the corect sequence and run only once]"
+        "given [all jobs finished] should [resolve in the corect sequence]"
+      )
+    }
+  )
+
+  t.end()
+})
+
+test("queue - dont reuse existing", async t => {
+  const todosQueue = buildQueue({ shouldReuseExisting: false })
+  const todosList = buildCrudList()
+
+  const createPromise1 = todosQueue.push({
+    fn: todosList.create,
+    params: [{ id: "1", value: "lorem" }],
+  })
+
+  const createPromise2 = todosQueue.push({
+    fn: todosList.create,
+    params: [{ id: "1", value: "lorem" }],
+  })
+
+  const createPromise3 = todosQueue.push({
+    fn: todosList.create,
+    params: [{ id: "2", value: "ipsum" }],
+  })
+
+  t.notEqual(
+    createPromise1,
+    createPromise2,
+    "given [multiple .push with same fn and params] should [return different promises]"
+  )
+
+  t.notEqual(
+    createPromise2,
+    createPromise3,
+    "given [multiple .push with same fn and different params] should [return different promises]"
+  )
+
+  await Promise.all([createPromise1, createPromise2, createPromise3]).then(
+    ([result1, result2, result3]) => {
+      t.deepEqual(
+        { id: "1", value: "lorem" },
+        result1,
+        "given [1st job finished] should [resolve to correct value]"
+      )
+
+      t.notEqual(
+        result1,
+        result2,
+        "given [1st and 2nd jobs finished from same fn and params] should [resolve to a different value]"
+      )
+
+      t.deepEqual(
+        { id: "2", value: "ipsum" },
+        result3,
+        "given [3rd job finished from different fn and params] should [resolve to correct value]"
+      )
+
+      t.deepEqual(
+        todosList.items(),
+        [
+          { id: "1", value: "lorem" },
+          { id: "1", value: "lorem" },
+          { id: "2", value: "ipsum" },
+        ],
+        "given [all jobs finished] should [resolve in the corect sequence]"
       )
     }
   )
